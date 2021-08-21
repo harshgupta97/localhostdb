eca60e7fe83f3471b0d2462e191e2130307836a9
Harsh Gupta <69745424+harshgupta97@users.noreply.github.com>
2021-08-21 13:17:18 +0530

    🎉 Feat: add a dist.bat file use to create dist zip file for end user

---
+++ dist.bat
@@ -0,0 +1,5 @@
+echo zipping...
+
+"C:\Program Files\7-Zip\7z.exe" a -tzip "D:\Work\ParallelSoftware\package\public\localhostdb\localhostdb.zip" "D:\Work\ParallelSoftware\package\public\localhostdb\"
+
+echo Done, Ready for distribution!
