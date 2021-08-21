66ee24c3b4cad7be8da28d4aeca8f04711da00b1
Harsh Gupta <69745424+harshgupta97@users.noreply.github.com>
2021-08-21 13:56:49 +0530

    🛠 Fix: now the dist.bat will build and archive with same command

--- dist.bat
+++ dist.bat
@@ -1,5 +1,5 @@
-echo zipping...
+echo  Creating dist files and zipping it for distribution...
 
-"C:\Program Files\7-Zip\7z.exe" a -tzip "D:\Work\ParallelSoftware\package\public\localhostdb\localhostdb.zip" "D:\Work\ParallelSoftware\package\public\localhostdb\"
+npm run build && "C:\Program Files\7-Zip\7z.exe" a -tzip "D:\Work\ParallelSoftware\package\public\localhostdb\localhostdb.zip" "D:\Work\ParallelSoftware\package\public\localhostdb\" -xr!node_modules -xr!src -xr!*.log -xr!*.md -xr!dist.bat -xr!.gitignore -xr!nodemon.json -xr!tsconfig.json -xr!localhostdb.zip -xr!clean.bat
 
 echo Done, Ready for distribution!
